6bfed3920902d63fe10c4187ac59dc63c5769e45
mariem <86363907+MarieMoalla@users.noreply.github.com>
2022-01-19 09:59:39 +0100

    First Commit

---
+++ Projet jeu/Assets/Mes éléments graphiques/Level_1/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+ #region variables
+ private float currentTime = 0f;
+ private float startingTime = 30f;
+ private int played = 0;
+
+ public AudioSource lose;
+ public AudioSource background;
+ public TextMeshProUGUI  timer;
+ public GameObject GameOver;
+ public GameObject HasWon;
+ public GameObject Paused;
+ public GameObject EditSound;
+ public GameObject instructions;
+ public Slider slider;
+ #endregion
+ 
+ #region methods
+
+ void Start()
+ {
+     currentTime = startingTime;
+ }
+
+ void Update()
+ {
+   
+     if(currentTime > 0 && HasWon.active == false && Paused.active == false && EditSound.active == false && instructions.active == false) 
+     {
+     currentTime -= 1*Time.deltaTime;
+     timer.SetText(((int)(currentTime)).ToString()) ;
+     }
+     else if(currentTime <= 0 && HasWon.active == false ) 
+     {
+         Debug.Log("Game Over");
+         GameOver.SetActive(true);
+         background.Pause();
+         played ++;
+         if(played ==1) lose.Play();
+     }    
+     
+ }
+#endregion
+}
